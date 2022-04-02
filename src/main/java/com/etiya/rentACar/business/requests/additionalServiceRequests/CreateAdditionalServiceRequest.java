413ae5b16051c4f8c77075d3e2caa6ca9577388b
Ali Çallı <alicalli0210@gmail.com>
2022-04-03 00:51:10 +0300

    03.04.2022-code refactor

--- src/main/java/com/etiya/rentACar/business/requests/additionalServiceRequests/CreateAdditionalServiceRequest.java
+++ src/main/java/com/etiya/rentACar/business/requests/additionalServiceRequests/CreateAdditionalServiceRequest.java
@@ -1,5 +1,6 @@
 package com.etiya.rentACar.business.requests.additionalServiceRequests;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateAdditionalServiceRequest {
-
+    @JsonIgnore
     private int id;
 
     private String name;
--- src/main/java/com/etiya/rentACar/business/responses/carResponses/ListCarDto.java
+++ src/main/java/com/etiya/rentACar/business/responses/carResponses/ListCarDto.java
@@ -1,7 +1,6 @@
 package com.etiya.rentACar.business.responses.carResponses;
 
 import com.etiya.rentACar.entities.CarStates;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,22 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ListCarDto {
     private int id;
-
     private double dailyPrice;
-
     private String description;
-
     private double modelYear;
-
-    private int cityName;
-
+    private String cityName;
     private String brandName;
-
     private String colorName;
-
     private CarStates carStateName;
-
-
 
 
 }
